6243e9016f209153a4b074913aa32e14e14da642
Fabio Ivona <fabio.ivona@defstudio.it>
2020-09-18 01:05:08 +0200

    wip

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -10,23 +10,23 @@ use Illuminate\View\ComponentAttributeBag;
 ?>
 
 <div class="row">
-    <div class="col-12">
-        <x-def-text name="{{$prefix}}[street]" label="{{ucwords(__('def-address::address.street'))}}"/>
+    <div class="col-12 form-group">
+        <x-text name="{{$prefix}}[street]" label="{{ucwords(__('def-address::address.street'))}}"/>
     </div>
 </div>
 <div class="row">
-    <div class="col-12 col-md-4">
-        <x-def-text name="{{$prefix}}[postal_code]" label="{{ucwords(__('def-address::address.postal_code'))}}"/>
+    <div class="col-12 col-md-4 form-group">
+        <x-text name="{{$prefix}}[postal_code]" label="{{ucwords(__('def-address::address.postal_code'))}}"/>
     </div>
-    <div class="col-12 col-md-6">
-        <x-def-text name="{{$prefix}}[city]" label="{{ucwords(__('def-address::address.city'))}}"/>
+    <div class="col-12 col-md-6 form-group">
+        <x-text name="{{$prefix}}[city]" label="{{ucwords(__('def-address::address.city'))}}"/>
     </div>
-    <div class="col-12 col-md-2">
-        <x-def-text name="{{$prefix}}[state]" label="{{ucwords(__('def-address::address.state'))}}"/>
+    <div class="col-12 col-md-2 form-group">
+        <x-text name="{{$prefix}}[state]" label="{{ucwords(__('def-address::address.state'))}}"/>
     </div>
 </div>
 <div class="row">
-    <div class="col-12">
-        <x-def-text name="{{$prefix}}[country]" label="{{ucwords(__('def-address::address.country'))}}"/>
+    <div class="col-12 form-group">
+        <x-text name="{{$prefix}}[country]" label="{{ucwords(__('def-address::address.country'))}}"/>
     </div>
 </div>
